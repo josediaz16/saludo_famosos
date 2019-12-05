2147517041709a81eea4218ae993ffcd986b695e
jose.luisdiaz <jose.luisdiaz@yello.co>
2019-12-05 15:42:39 -0500

    adds application contract

---
+++ app/contracts/application_contract.rb
@@ -0,0 +1,6 @@
+class ApplicationContract < Dry::Validation::Contract
+  config.messages.default_locale = :es
+  config.messages.backend = :i18n
+  config.messages.load_paths << 'config/errors.yml'
+
+end
