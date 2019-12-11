bb5b9df864cfd393b41dcc1991a4fb7a0e19faa2
jose.luisdiaz <jose.luisdiaz@yello.co>
2019-12-11 17:37:52 -0500

    config database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,13 +6,12 @@ ENV['HOME'] = "spec/fixtures/home"
 require "byebug"
 require "fileutils"
 require "jets"
+require 'database_cleaner'
 
 abort("The Jets environment is running in production mode!") if Jets.env == "production"
 Jets.boot
 
 require "jets/spec_helpers"
-
-
 
 module Helpers
   def payload(name)
@@ -20,6 +19,19 @@ module Helpers
   end
 end
 
+DatabaseCleaner.allow_remote_database_url = true
+
 RSpec.configure do |c|
   c.include Helpers
+  c.include FactoryBot::Syntax::Methods
+
+  c.before(:suite) do
+    FactoryBot.find_definitions
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  c.around(:each) do |example|
+    DatabaseCleaner.cleaning(&example)
+  end
 end
