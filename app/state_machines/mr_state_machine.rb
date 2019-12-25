a9b86442fa57c3c9ec5444e1913811b9054311a1
Jose Diaz <jldiaz1612@gmail.com>
2019-12-25 12:31:58 -0500

    adds mr state machine

---
+++ app/state_machines/mr_state_machine.rb
@@ -0,0 +1,20 @@
+# Message Request State Machine
+class MrStateMachine
+  include Statesman::Machine
+
+  state :pending, initial: true
+  state :purchased
+  state :accepted
+  state :rejected
+  state :cancelled
+  state :delivered
+
+  transition from: :pending,   to: [:purchased, :cancelled]
+  transition from: :purchased, to: [:accepted, :rejected, :cancelled, :delivered]
+  transition from: :accepted,  to: [:cancelled, :delivered]
+  transition from: :cancelled, to: [:pending]
+
+  after_transition do |model, transition|
+    model.update(status: transition.to_state)
+  end
+end
